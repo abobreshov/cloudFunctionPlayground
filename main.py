58f2004227f7278074a8760d26d14b5dfbe94f77
abobreshov <skybobs47@gmail.com>
2022-08-04 11:31:12 +0100

    add ip to response

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import socket
+
+
 def main(request):
     """Responds to any HTTP request.
     Args:
@@ -7,10 +10,12 @@ def main(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
     request_json = request.get_json()
     if request.args and 'message' in request.args:
-        return request.args.get('message')
+        return request.args.get('message') + ' from ' + local_ip
     elif request_json and 'message' in request_json:
-        return request_json['message']
+        return request_json['message'] + ' from ' + local_ip
     else:
-        return f'Hello World!'
+        return f'Hello {hostname}! {local_ip}'
